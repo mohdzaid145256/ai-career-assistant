191b15129cfc28646950375d5cd5831e596798b2
Mohd Zaid <mohdzaid4919@gmail.com>
2026-06-21 09:57:44 +0530

    Fix main.py indentation

--- main.py
+++ main.py
@@ -9,22 +9,21 @@ from api.v1.resume import router as resume_router
 from api.v1.ats import router as ats_router
 from api.v1.interview import router as interview_router
 from api.v1.interview_answer import (
-router as interview_answer_router
+    router as interview_answer_router
 )
 from api.v1.resume_review import (
-router as resume_review_router
+    router as resume_review_router
 )
 from api.v1.resume_match import (
-router as resume_match_router
+    router as resume_match_router
 )
 
 # Create tables automatically
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-title="AI Career Assistant",
-version="1.0.0"
+    title="AI Career Assistant",
+    version="1.0.0"
 )
 
 app.include_router(auth_router)
@@ -35,9 +34,9 @@ app.include_router(interview_answer_router)
 app.include_router(resume_review_router)
 app.include_router(resume_match_router)
 
+
 @app.get("/")
 def root():
-return {
-"message": "AI Career Assistant API Running"
-}
-
+    return {
+        "message": "AI Career Assistant API Running"
+    }
